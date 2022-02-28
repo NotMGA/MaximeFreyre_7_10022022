0ec8c61095f811b5409c272bbd78143924d3bf5a
NotMGA <maximefreyre@gmail.com>
2022-02-28 11:25:57 +0100

    3 carac serach bar

--- Java/Algo/search.js
+++ Java/Algo/search.js
@@ -3,15 +3,19 @@ const recette = document.querySelectorAll("article");
 let filter_recipe =[];
 // document.querySelector(".btn").addEventListener("click" ,()=>{
     document.getElementById("search_bar").addEventListener("keyup",function(e){
-    const search_bar = document.getElementById("search_bar").value.toLowerCase();
-     filter_recipe= recipes.filter(character =>{
-        // console.log(character.ingredients[0].ingredient)
-        ingredient_var = [];
-        for(let i=0 ; i< character.ingredients.length ;i++){
-            ingredient_var = ingredient_var + character.ingredients[i].ingredient;
-            }
-        return(character.name.toLowerCase().includes(search_bar) || character.appliance.toLowerCase().includes(search_bar) ||character.ustensils.includes(search_bar) || ingredient_var.includes(search_bar) || character.description.includes(search_bar) )
-})
+        if (document.getElementById("search_bar").value.length >= 3)
+        {
+            const search_bar = document.getElementById("search_bar").value.toLowerCase();
+            filter_recipe= recipes.filter(character =>{
+               // console.log(character.ingredients[0].ingredient)
+               ingredient_var = [];
+               for(let i=0 ; i< character.ingredients.length ;i++){
+                   ingredient_var = ingredient_var + character.ingredients[i].ingredient;
+                   }
+               return(character.name.toLowerCase().includes(search_bar) || character.appliance.toLowerCase().includes(search_bar) ||character.ustensils.includes(search_bar) || ingredient_var.includes(search_bar) || character.description.includes(search_bar) )
+       })
+        }
+
 
     // console.log(filter_recipe);
     clearData();
