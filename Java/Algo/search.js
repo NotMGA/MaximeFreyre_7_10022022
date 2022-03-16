258d144d090f14a93fcd35040cc080d98059fe84
NotMGA <maximefreyre@gmail.com>
2022-03-16 12:43:40 +0100

    16/03/2022

--- Java/Algo/search.js
+++ Java/Algo/search.js
@@ -1,26 +1,28 @@
 const recette = document.querySelectorAll("article");
-// let ingredient_var = [];
+
 let filter_recipe =[];
-// document.querySelector(".btn").addEventListener("click" ,()=>{
+
     document.getElementById("search_bar").addEventListener("keyup",function(e){
         if (document.getElementById("search_bar").value.length >= 3 || document.getElementById("search_bar").value.length == 0 )
         {
             const search_bar = document.getElementById("search_bar").value.toLowerCase();
             filter_recipe= recipes.filter(character =>{
-               // console.log(character.ingredients[0].ingredient)
+
                ingredient_var = [];
                for(let i=0 ; i< character.ingredients.length ;i++){
                    ingredient_var = ingredient_var + character.ingredients[i].ingredient;
                    }
                return(character.name.toLowerCase().includes(search_bar) || character.appliance.toLowerCase().includes(search_bar) ||character.ustensils.includes(search_bar) || ingredient_var.includes(search_bar) || character.description.includes(search_bar) )
        })
+       if (filter_recipe.length == 0){
+        document.querySelector(".msg_vide").style.display = "flex";
+        clearData();
+    }
+    else{
+     document.querySelector(".msg_vide").style.display = "none";
+     clearData();
+     filter_select();
+     }
         }
 
-
-    // console.log(filter_recipe);
-    clearData();
-    filter_select();
-    // displayData(selected_recipe);
-    // displayData(filter_recipe);
-    
 })
